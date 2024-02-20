6781f339c23c46a6bd9075aeaa82799b69c050de
e-Fang.Liu1 <e.Fang.Liu1@geely.com>
2024-02-20 16:21:11 +0800

    fix: tttt

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import prefetchPlugin from 'vite-plugin-bundle-prefetch';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/myResume',
   plugins: [
     vue(),
     compression(),
@@ -23,7 +24,7 @@ export default defineConfig({
       customSplitting: {
         // `vue` 会被打包到一个名为`vue-vendor`的 chunk 里面(包括它们的一些依赖，如 object-assign)
         'vue-vendor': [/vue/],
-        'tailwind-vendor': [/tailwindcss/],        
+        'tailwind-vendor': [/tailwindcss/],
         // 源码中目录的代码都会打包进 [name] 这个 chunk 中
         'styles': [/src\/styles/],
         'locales': [/src\/locales/]
